7b19e2dc8499fa8f256ccd9f29e9412139f7d57a
mrdark69 <mrdark6996@gmail.com>
2016-02-17 23:31:11 +0700

    6

---
+++ resources/views/frontend/layouts/content.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en" class="no-js">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+    @include('frontend.includes.head')
+</head>
+<!-- END HEAD -->
+
+<!-- BEGIN BODY -->
+<body>
+
+<div class="wrapper">
+    <!--=== Header ===-->
+    @include('frontend.includes.header')
+<!--=== End Header ===-->
+    @include('frontend.includes.slider')
+    @include('frontend.includes.slider_level2')
+
+
+    <!-- BEGIN PAGE CONTAINER -->
+    <div class="container content-sm">
+
+        @yield('content')
+
+
+        @include('frontend.includes.client')
+
+    </div>
+    <!-- END PAGE CONTAINER -->
+
+
+</div>
+
+
+
+
+@include('frontend.includes.footer')
+
+</body>
+<!-- END BODY -->
+</html>
