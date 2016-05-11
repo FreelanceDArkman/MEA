5cd6eff3fe31806ea422f75feaf72531d81f1a15
mrdark69 <mrdark6996@gmail.com>
2016-05-11 22:36:49 +0700

    aasdasd

--- resources/views/backend/pages/ajax/ajax_53_2.blade.php
+++ resources/views/backend/pages/ajax/ajax_53_2.blade.php
@@ -42,8 +42,8 @@
 
 
                 <td>{{($item->NEWS_TOPIC_FLAG == "0" ? "Active" : "Inactive")}}</td>
-                <td>{{get_date_notime($item->START_DATE) }}</td>
-                <td>{{get_date_notime($item->EXPIRE_DATE) }}</td>
+                <td>{{get_date_notime_en($item->START_DATE) }}</td>
+                <td>{{get_date_notime_en($item->EXPIRE_DATE) }}</td>
 
 
             </tr>
